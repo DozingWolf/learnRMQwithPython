690f0bc5dded6df6563ecee4c27ddb3e6992a38e
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2018-12-11 17:15:27 +0800

    ok

--- test_conn_PUB_SUB.py
+++ test_conn_PUB_SUB.py
@@ -13,22 +13,24 @@ para = pika.ConnectionParameters(host='10.62.24.70',credentials=crea,heartbeat_i
 conn = pika.BlockingConnection(parameters=para)
 #创建频道
 channel = conn.channel()
-#创建队列
-#申明durable表示要求持久化
-channel.queue_declare(queue='hello_q_durable_e2',durable=True)
-#持久化需要使用delivery_mode=2
+#此处由消费端生成随机队列，利用绑定进行
+ #创建队列
+ #申明durable表示要求持久化
+ #channel.queue_declare(queue='hello_q_durable_e2',durable=True)
+ #持久化需要使用delivery_mode=2
 para_dur = pika.BasicProperties(delivery_mode=2)
 #申明交换机
-exchange_name = 'TEST_EXCHANGE'
-exchange_type = 'fanout'
-exchange_durable = True
-channel.exchange_declare(exchange=exchange_name,exchange_type=exchange_type)
+exc_name = 'TEST_EXCHANGE'
+exc_type = 'fanout'
+exc_durable = True
+channel.exchange_declare(exchange=exc_name,exchange_type=exc_type)
+
 #输入想要传输的内容
 a = 1
 msg = 'k'
 for a in range(100):
     msg = msg+'k'
-    channel.basic_publish(exchange=exchange_name,routing_key='hello_q_durable',body=msg,properties=para_dur)
+    channel.basic_publish(exchange=exc_name,routing_key='hello_q_durable',body=msg,properties=para_dur)
     print(a)
 
 #输出状态
